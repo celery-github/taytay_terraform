3efcb84bc7422ca026af9877aa29386fb2e111e1
celery-github <112822750+celery-github@users.noreply.github.com>
2023-12-28 10:39:32 -0500

    removed notes from main.tf

--- terraform/feature_switch/main.tf
+++ terraform/feature_switch/main.tf
@@ -11,12 +11,3 @@ resource "aws_instance" "ec2" {
     instance_type = "t2.micro"
     count = var.environment == "prod" ? 1 : 0 #if environment = prod then set the value to 1
 }
-
-#what if we want a new resource in dev, and not prod?
-#turnery operator 
-# In this example line 12, for count, create one EC2 instance, if it's equal to prod, but if it's not equal to prod, then don't create anything.
-
-# run this command ###-- terraform plan -var-file="test.tfvars"
-    #nothing happens
-# run this command ###-- terraform plan -var-file="prod.tfvars"
-    #it creates one instance 
