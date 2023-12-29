205f0366fa35c97c2416862caaadacb64a0d0882
celery-github <112822750+celery-github@users.noreply.github.com>
2023-12-29 10:23:36 -0500

    Created security group tf file for aws

---
+++ terraform/security_group/sg_main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+    region = "eu-west-2"
+}
+
+resource "aws_instance" "ec2" {
+    ami = "ami-032598fcc7ed91c7a"
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.webtraffic.name]           #added in
+}
+
+resource "aws_security_group" "webtraffic" {
+    name = "Allow HTTPS"
+
+    ingress{
+        from_port = 443
+        to_port = 443
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    } 
+
+    egress {
+        from_port = 443
+        to_port = 443
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    } 
+    }
+
+#how to create a security group
+
+#terraform init
+#terraform apply
+#terraform destroy
+# option + command + S
