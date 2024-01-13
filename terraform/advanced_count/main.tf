9992a340b0c0bdf16d122b70fd3d5d83e58920ab
celery-github <112822750+celery-github@users.noreply.github.com>
2024-01-12 19:38:15 -0500

    Created main.tf

---
+++ terraform/advanced_count/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "eu-west-2"
+  
+}
+
+module "db" {
+    source = "./db"
+    server_names = ["mariadb","mysql","mssql"]   
+}
+
+output "private_ips" {
+    value = module.db.PrivateIP     #the output from db.tf "PrivateIP"
+}
