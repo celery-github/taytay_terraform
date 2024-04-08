9e87fa3e15bc091616bf2d8eeea4d9ebeb222572
celery-github <112822750+celery-github@users.noreply.github.com>
2024-04-08 17:54:36 -0400

    Create main.tf

---
+++ terraform/databases/database/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+    region = "eu-west-2"
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-032598fcc7e9d1c7a"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "Web Server"
+    }
+    depends_on = [ aws_instance.myec2db ]
+}
+
+resource "aws_instance" "myec2db" {
+    ami = "ami-032598fcc7e9d1c7a"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "DB Server"
+    }
+}
+
+data "aws_instance" "dbsearch" {
+    filter {
+      name = "tag:Name"
+      values = ["DB Server"]
+    }
+}
+
+output "dbservers" {
+    value = data.aws_instance.dbsearch.availability_zone
+
+}
+
+#MORE INFO https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/instance.html
